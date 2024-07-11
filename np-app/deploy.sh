78b508cd8853ace368d18a5e44fee60b0d0feab8
NigelGoh16 <nigell.goh@gmail.com>
2024-07-11 14:23:36 +0800

    Update build command

--- np-app/deploy.sh
+++ np-app/deploy.sh
@@ -2,7 +2,7 @@ echo "Switching to branch main"
 git checkout main
 
 echo "Building App..."
-next run build
+next build
 
 echo "Deploying files to server..."
 scp -r build/* root@157.245.70.171:/var/www/157.245.70.171/
